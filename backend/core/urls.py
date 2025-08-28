8c87954f149073825b1997a45739039e02dcc0e6
Andrey <shooroop87@mail.ru>
2025-08-29 02:42:10 +0400

    v4
    v4

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("sustainability/", views.sustainability, name="sustainability"),
     path("contacts/", views.contacts, name="contacts"),
+    path("faq/", views.faq, name="faq"),
+    path("shipping-delivery/", views.shipping_delivery, name="shipping_delivery"),
+    path("legal/", views.legal, name="legal"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("accessibility/", views.accessibility, name="accessibility"),
+    path("sitemap/", views.sitemap, name="sitemap"),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -14,6 +14,24 @@ def sustainability(request):
 def contacts(request):
     return render(request, "core/contacts.html")
 
+def faq(request):
+    return render(request, "core/faq.html")
+
+def shipping_delivery(request):
+    return render(request, "core/shipping_delivery.html")
+
+def legal(request):
+    return render(request, "core/legal.html")
+
+def privacy(request):
+    return render(request, "core/privacy.html")
+
+def accessibility(request):
+    return render(request, "core/accessibility.html")
+
+def sitemap(request):
+    return render(request, "core/sitemap.html")
+
 class HealthView(View):
     def get(self, request):
         return JsonResponse({"status":"ok"})
